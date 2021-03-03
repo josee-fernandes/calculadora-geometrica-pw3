2f6b7da2376c86eb5ec8d808cee30d58dadead4b
José Fernandes <jose-vitor-7@hotmail.com>
2021-03-03 20:43:24 -0300

    Rotas do losango

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@ Route::get('/', function () {
 });
 
 
+/* ===  LOSANGO === */
+
+Route::get('/losango/perimetro/{lado}/', function($lado){
+    $retorno = new stdClass();
+    $retorno->forma = 'Losango';
+    $retorno->calculo = 'Perímetro';
+    $retorno->lado = $lado;
+    $retorno->resultado = $lado * 4;
+    return view('result', ['resultado' => $retorno]);
+});
+
+Route::get('/losango/area/{lado}/{altura}', function($lado, $altura){
+    $retorno = new stdClass();
+    $retorno->forma = 'Losango';
+    $retorno->calculo = 'Área';
+    $retorno->lado = $lado;
+    $retorno->altura = $altura;
+    $retorno->resultado = $lado * $altura;
+    return view('result', ['resultado' => $retorno]);
+});
